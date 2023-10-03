f7e747a0e9195b35ff06e57123973582acc7458b
agatti4 <ajgatti4@gmail.com>
2023-10-03 15:36:16 -0400

    Refactor code into morefunctions

--- src/main.rs
+++ src/main.rs
@@ -8,60 +8,34 @@ fn get_input() -> Vec<String> {
     return args;
 }
 
-
-
-
-fn main() {
-    let args = get_input();
-
-   if args.len() != 2 {
-        eprintln!("Usage: {} <file_path>", args[0]);
-        return;
-    } 
-
-    let file_path : &String = &args[1];
-    println!("In file {}", file_path);
-
-    let contents : String = fs::read_to_string(&file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{}", contents);
-
-    let lines: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
-
-        // Check if there are at least four lines in the file
+fn parse_file_content(lines: Vec<String>) -> Result<(i32, i32, i32), String> {
     if lines.len() < 4 {
-        eprintln!("File does not contain enough lines");
-        return;
+        return Err("File does not contain enough lines".to_string());
     }
-
+    
     // Parse the first line into num_rows
     let num_rows: i32 = match lines[0].trim().parse() {
         Ok(value) => value,
-        Err(_) => {
-            eprintln!("Error parsing num_rows");
-            return;
-        }
-    }; 
+        Err(_) => return Err("Error parsing num_rows".to_string()),
+    };
 
     // Parse the second line into num_cols
-    let num_cols: i32 = match lines[0].trim().parse() {
+    let num_cols: i32 = match lines[1].trim().parse() {
         Ok(value) => value,
-        Err(_) => {
-            eprintln!("Error parsing num_cols");
-            return;
-        }
+        Err(_) => return Err("Error parsing num_cols".to_string()),
     };
 
     // Parse the third line into num_iters
     let num_iters: i32 = match lines[2].trim().parse() {
         Ok(value) => value,
-        Err(_) => {
-            eprintln!("Error parsing num_iters");
-            return;
-        }
+        Err(_) => return Err("Error parsing num_iters".to_string()),
     };
 
+    Ok((num_rows, num_cols, num_iters))
+
+}
+
+fn start_game(num_rows: i32, num_cols: i32, num_iters: i32) {
     println!("num_rows: {}", num_rows);
     println!("num_cols: {}", num_cols);
     println!("num_iters: {}", num_iters);
@@ -73,7 +47,34 @@ fn main() {
         }
         println!(); // Move to the next row
     }
- 
+}
+
+fn main() {
+    let args = get_input();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        return;
+    } 
+
+    let file_path : &String = &args[1];
+    println!("In file {}", file_path);
+
+    let contents : String = fs::read_to_string(&file_path)
+        .expect("Should have been able to read the file");
+
+    let lines: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
+    
+    let (num_rows, num_cols, num_iters) = match parse_file_content(lines) {
+        Ok((num_rows, num_cols, num_iters)) => (num_rows, num_cols, num_iters),
+        Err(error) => {
+            eprintln!("{}", error);
+            return;
+        }
+    };
+
+    start_game(num_rows, num_cols, num_iters);
+
 }
 
 // Ask for input file that contains num rows, num cols, num iterations
