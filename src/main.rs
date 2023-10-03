ad5bd951fbe5da9acd8a05667354a3a4bbffaad6
agatti4 <ajgatti4@gmail.com>
2023-10-02 21:45:08 -0400

    Add file read in

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use std::env;
 use std::fs;
 
+fn get_input() -> Vec<String> {
+    // Get the first command-line argument (assuming it's the file path)
+    let args: Vec<String> = env::args().collect();
+
+    return args;
+}
+
+
+
+
 fn main() {
-    println!("Hello, world!");
-    let file_path = "src/gliderGun.txt";
+    let args = get_input();
+
+   if args.len() != 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        return;
+    } 
+
+    let file_path : &String = &args[1];
     println!("In file {}", file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents : String = fs::read_to_string(&file_path)
         .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    println!("With text:\n{}", contents);
 }
 
 // Ask for input file that contains num rows, num cols, num iterations
