0892dc60404eec614c782a55b85c8ad495a45081
agatti4 <ajgatti4@gmail.com>
2023-10-02 22:28:27 -0400

    Store number of rows, columns, and iterations from file in variables

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,49 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text:\n{}", contents);
+
+    let lines: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
+
+        // Check if there are at least four lines in the file
+    if lines.len() < 4 {
+        eprintln!("File does not contain enough lines");
+        return;
+    }
+
+    // Parse the first line into num_rows
+    let num_rows: i32 = match lines[0].trim().parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Error parsing num_rows");
+            return;
+        }
+    }; 
+
+    // Parse the second line into num_cols
+    let num_cols: i32 = match lines[0].trim().parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Error parsing num_cols");
+            return;
+        }
+    };
+
+
+
+    // Parse the third line into num_iters
+    let num_iters: i32 = match lines[2].trim().parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Error parsing num_iters");
+            return;
+        }
+    };
+
+    println!("num_rows: {}", num_rows);
+    println!("num_cols: {}", num_cols);
+    println!("num_iters: {}", num_iters);
+    
+ 
 }
 
 // Ask for input file that contains num rows, num cols, num iterations
