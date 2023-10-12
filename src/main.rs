2b0469e620dc3bf425c6962e73247c2ae4be8d39
agatti4 <ajgatti4@gmail.com>
2023-10-11 23:52:24 -0400

    Add create_init_board function and pass board to start game

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn get_input() -> Result<Vec<String>, String> {
     Ok(lines)
 }
 
-fn parse_file_content(lines: Vec<String>) -> Result<(i32, i32, i32), String> {
+fn parse_file_content(lines: &Vec<String>) -> Result<(i32, i32, i32), String> {
     if lines.len() < 4 {
         return Err("File does not contain enough lines".to_string());
     }
@@ -46,18 +46,35 @@ fn parse_file_content(lines: Vec<String>) -> Result<(i32, i32, i32), String> {
 
 }
 
-fn start_game(num_rows: i32, num_cols: i32, num_iters: i32) {
+fn create_init_board(num_rows: i32, num_cols: i32, lines: &Vec<String>) -> Result<Vec<Vec<char>>, String> {
+    let mut grid = vec![vec!['x'; num_cols as usize]; num_rows as usize];
+
+    for line in lines.iter().skip(3) {
+        let parts: Vec<&str> = line.split(' ').collect();
+        if parts.len() == 2 {
+            if let (Ok(row), Ok(col)) = (parts[0].parse::<usize>(), parts[1].parse::<usize>()) {
+                if row < num_rows as usize && col < num_cols as usize {
+                    grid[row][col] = '@';
+                }
+            }
+        }
+    }
+
+    Ok(grid)
+}
+
+fn start_game(num_rows: i32, num_cols: i32, num_iters: i32, grid: &Vec<Vec<char>>) {
     println!("num_rows: {}", num_rows);
     println!("num_cols: {}", num_cols);
     println!("num_iters: {}", num_iters);
-    
-    // Create and print the 2D grid of 'x's
-    for _ in 0..num_rows {
-        for _ in 0..num_cols {
-            print!("x ");
+
+    for row in grid {
+        for cell in row {
+            print!("{}", cell);
         }
-        println!(); // Move to the next row
+        println!();
     }
+   
 }
 
 fn main() {
@@ -71,7 +88,7 @@ fn main() {
     }; 
     
     // Parse input file
-    let (num_rows, num_cols, num_iters) = match parse_file_content(lines) {
+    let (num_rows, num_cols, num_iters) = match parse_file_content(&lines) {
         Ok((num_rows, num_cols, num_iters)) => (num_rows, num_cols, num_iters),
         Err(error) => {
             eprintln!("{}", error);
@@ -79,8 +96,16 @@ fn main() {
         }
     };
     
+    let grid: Vec<Vec<char>> = match create_init_board(num_rows, num_cols, &lines) {
+        Ok(grid) => grid,
+        Err(error) => {
+            eprintln!("{}", error);
+            return;
+        }
+    }; 
+
     // Start game with data from file
-    start_game(num_rows, num_cols, num_iters);
+    start_game(num_rows, num_cols, num_iters, &grid);
 
 }
 
